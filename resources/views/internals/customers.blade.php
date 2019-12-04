b59f5056aad752911804d2576c3a722818547618
Martin <martin_aj22@hotmail.com>
2019-12-03 21:43:37 -0600

    menu nav

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -1,3 +1,11 @@
+<ul>
+    <li><a href="/">Home<Home/a></li>
+    <li><a href="about">About</a></li>
+    <li><a href="contact">Contact Us</a></li>
+    <li><a href="customers">Customers</a></li>
+</ul>
+
+
 <h1>Costumers</h1>
 
 <ul>
