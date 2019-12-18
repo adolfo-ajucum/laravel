649cd3a902d47a39a21ad6501595b8ff8ea3480e
Martin <martin_aj22@hotmail.com>
2019-12-18 17:35:42 -0600

    style: uso de bootsrap
    Se agregaron elementos de bootstrap
    para mejorar la interfaz

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -1,16 +1,24 @@
 @extends('layout')
 
 @section('content')
-    <h1>Costumers</h1>
-    <form action="customers" method="POST" class="pb-5">
-        <p>Name: </p>
-        <div class="input-group pb-2">
-        <input type="text" name="name" value={{old('name')}}>
+    <div class="row">
+        <div class="col-12">
+        <h1>Costumers</h1>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-12">
+        <form action="customers" method="POST">
+        <label for="name">Name</label>
+        <div class="form-group">
+        <input type="text" name="name" value="{{old('name')}}" class="form-control">
         </div>  
 
-        <p>Email: </p>
-        <div class="input-group pb-2">
-        <input type="email" name="email" value={{old('email')}}>
+        <label for="email">Email</label>
+        <div class="form-group">
+        <input type="email" name="email" value="{{old('email')}}" class="form-control">
         </div>
         @if ($errors->any())
           <ul>
@@ -19,14 +27,20 @@
               @endforeach
           </ul>
         @endif
-        <button type="submit">Add Customer</button>
+        <button type="submit" class="btn btn-primary">Add Customer</button>
 
         @csrf
     </form>
-
+        </div>
+    </div>
+<hr>
+<div class="row">
+    <div class="col-12">
     <ul>
         @foreach ($customers as $customer)
         <li>{{ $customer->name }} <span class="text-muted"> {{ $customer->email}}</span></li>
         @endforeach
     </ul>
+    </div>
+</div>
 @endsection
