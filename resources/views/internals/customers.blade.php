71d0b840eaf23558371d173b8701feff0ce733ce
Martin <martin_aj22@hotmail.com>
2019-12-18 23:50:30 -0600

    feat: opcion de estado para customers

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -22,6 +22,15 @@
         <div class="form-group">
         <input type="email" name="email" value="{{old('email')}}" class="form-control">
         </div>
+
+        <div class="form-group">
+        <label for="active">Status</label>
+        <select name="active" id="active" class="form-control">
+            <option value="" disabled>Select customer status</option>
+            <option value="1">Active</option>
+            <option value="0">Inactive</option>
+        </select>
+        </div>
         @if ($errors->any())
           <ul>
               @foreach ($errors->all() as $error)
@@ -37,12 +46,22 @@
     </div>
 <hr>
 <div class="row">
-    <div class="col-12">
+    <div class="col-6">
+    <h3>Active customers</h3>
     <ul>
-        @foreach ($customers as $customer)
-        <li>{{ $customer->name }} <span class="text-muted"> {{ $customer->email}}</span></li>
+        @foreach ($activeCustomers as $activeCustomers)
+        <li>{{ $activeCustomers->name }} <span class="text-muted"> {{ $activeCustomers->email}}</span></li>
         @endforeach
     </ul>
     </div>
+
+<div class="row">
+    <div class="col-6">
+    <h3>Inactive customers</h3>
+    <ul>
+        @foreach ($inactiveCustomers as $inactiveCustomers)
+        <li>{{ $inactiveCustomers->name }} <span class="text-muted"> {{ $inactiveCustomers->email}}</span></li>
+        @endforeach
+    </ul>
 </div>
 @endsection
